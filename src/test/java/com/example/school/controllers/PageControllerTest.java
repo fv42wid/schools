287af1e2208a114c374b2d4cfac4231fb2e2ee02
Frank Vinci <fv42wid@yahoo.com>
2018-02-07 14:17:48 -0500

    fix login form, update controller test to run with custom security config

--- src/test/java/com/example/school/controllers/PageControllerTest.java
+++ src/test/java/com/example/school/controllers/PageControllerTest.java
@@ -1,33 +1,46 @@
 package com.example.school.controllers;
 
+import com.example.school.config.SpringSecConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(PageController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+//@WebMvcTest(PageController.class)
 public class PageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    //@Autowired
+    //private WebApplicationContext context;
+
 
     @Test
     public void testIndexPage() throws Exception {
-        mockMvc.perform(get("/"))
+        mockMvc.perform(get(""))
                 .andExpect(status().isOk())
-                .andExpect(view().name("hom"))
-                .andExpect(content().string(containsString("Hello")));
+                .andExpect(view().name("pages/index"))
+                .andExpect(content().string(containsString("Welcome")));
     }
 
 }
