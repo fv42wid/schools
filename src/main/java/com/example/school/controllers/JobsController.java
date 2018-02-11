d28f666063ad5bcfd2e3700b6244864dc4410e5c
Frank Vinci <fv42wid@yahoo.com>
2018-02-11 15:45:26 -0500

    finish jobs#save, update next

--- src/main/java/com/example/school/controllers/JobsController.java
+++ src/main/java/com/example/school/controllers/JobsController.java
@@ -1,12 +1,19 @@
 package com.example.school.controllers;
 
 import com.example.school.commands.JobCommand;
+import com.example.school.models.User;
 import com.example.school.services.JobService;
 import com.example.school.services.UserRepositoryUserDetailsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/jobs")
@@ -30,5 +37,16 @@ public class JobsController {
     public String newJob(Model model) {
         model.addAttribute("job", new JobCommand());
         return "jobs/form";
+    }
+
+    @PostMapping({"", "/"})
+    public String saveOrUpdate(@Valid @ModelAttribute("job") JobCommand command, Errors errors, Principal principal) {
+        if(errors.hasErrors()) {
+            return "jobs/form";
+        }
+
+        User currentUser = userService.loadUserByUsername(principal.getName());
+        JobCommand savedCommand = jobService.saveJobCommand(command, currentUser);
+        return "redirect:/jobs";
     }
 }
